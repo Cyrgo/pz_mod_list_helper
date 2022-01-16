7bad91de7bbf55b44ef6440a3db90b3d0554414d
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-15 18:26:53 -0600

    Ver 0.2: Adds empty line to please the IDE.

--- path_input.py
+++ path_input.py
@@ -3,6 +3,15 @@ def ini_path_input():
     print("Enter the path of your Project Zomboid Server Configuration ini file.")
     print("Best to open the file from file explorer,")
     print("copy paste the address, and add the Server Configuration ini file name.")
-    print("Example: C:\\Users\\cyrgo\\Zomboid\\Server\\Test Server.ini")
+    print("Example: C:\\Users\\cyrgo\\Zomboid\\Server\\31 Days Later-With Adi.ini")
     ini_path = input()
     return ini_path
+
+
+def client_workshop_path_input():
+    print("Enter the path of your Project Zomboid client Steam Workshop file.")
+    print("Best to open the file from file explorer,")
+    print("copy paste the address, and add the .acf file extension.")
+    print("Example: D:\\SteamLibrary\\steamapps\\workshop\\appworkshop_108600.acf")
+    acf_path = input()
+    return acf_path
