b3084e0f13faa6260b04c7bb22595c5fbaed7a6a
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-24 00:16:04 -0600

    Ver 0.2: Adds function to force update mods for PZ Server to fix "Workshop item version different than server's" error.

--- mod_update_fixer.py
+++ mod_update_fixer.py
@@ -1,3 +1,9 @@
+import path_utilities
 import shutil
 
-# This class will hold a function to force update mods for a server when Workshop Items are updated for the client
+
+# This function force updates Project Zomboid Server Steam Workshop Content File from the Client folder with any
+# updated mods that don't match with the Server's mods that have not been updated yet and throws the ever annoying
+# error message "Workshop item version different than server's".
+def force_update():
+    shutil.copy(path_utilities.client_workshop_path_input(), path_utilities.server_workshop_path_input())
