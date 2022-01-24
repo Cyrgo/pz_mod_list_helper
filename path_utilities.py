f4b00deb1f5a1ac803dcadf070f23e9918b45018
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-24 00:09:27 -0600

    Ver 0.2: Adds commenting to client_workshop_path_input() and creates new function to grab location of PZ Server Workshop Folder.

--- path_utilities.py
+++ path_utilities.py
@@ -8,6 +8,8 @@ def ini_path_input():
     return ini_path
 
 
+# This function takes in the string path of Project Zomboid Client Steam Workshop Content File and returns it to be
+# used.
 def client_workshop_path_input():
     print("Enter the path of your Project Zomboid client Steam Workshop file.")
     print("Best to open the file from file explorer,")
@@ -15,3 +17,14 @@ def client_workshop_path_input():
     print("Example: D:\\SteamLibrary\\steamapps\\workshop\\appworkshop_108600.acf")
     acf_path = input()
     return acf_path
+
+
+# This function takes in the string path of Project Zomboid Server Steam Workshop Content Folder and returns it to be
+# used.
+def server_workshop_path_input():
+    print("Enter the path of your Project Zomboid server Steam Workshop folder.")
+    print("Best to open the folder from file explorer,")
+    print("and copy paste the address.")
+    print("Example: D:\\SteamLibrary\\steamapps\\common\\ProjectZomboid\\steamapps\\workshop")
+    workshop_folder_path = input()
+    return workshop_folder_path
