c3d4125a5f0acf96834c2086ad1c272b91ccae54
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-26 03:12:17 -0600

    Ver 0.3: Adds drive input return and adds slight debugging to message box.

--- ui.py
+++ ui.py
@@ -9,23 +9,27 @@ main_window.wm_iconbitmap('images/icon.ico')
 main_window.title("PZ Server Mod Force Updater")
 main_window.geometry("380x140+10+20")
 
+drive_var = tkinter.StringVar()
+drive_entry = tkinter.Entry(main_window, textvariable=drive_var)
+
 
 def alert():
-    messagebox.showinfo("Hello", "World")
+    messagebox.showinfo("Hello", drive_entry.get())
 
 
 def submit():
 
+    drive_name = drive_entry.get()
+
     alert()
+
+    return drive_name
 
 
 main_lbl = Label(main_window, text=output_utilities.welcome_message(), font=("calibre", 10, "bold"))
 
-drive_var = tkinter.StringVar()
-drive_entry = tkinter.Entry(main_window, textvariable=drive_var)
-
 spiffo = PhotoImage(file="images/spiffo-button.png")
-submit_btn = tkinter.Button(main_window, text="Submit", command=submit, image=spiffo)
+submit_btn = tkinter.Button(main_window, command=submit, image=spiffo)
 
 main_lbl.pack(side="top")
 drive_entry.pack(side="top")
