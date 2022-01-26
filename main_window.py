3baf2c908e89ab81d82b95931c3fde21781fba19
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-26 16:14:23 -0600

    Ver 0.4: Adds commenting to all functions and code blocks.

--- main_window.py
+++ main_window.py
@@ -4,16 +4,19 @@ import path_utilities
 import tkinter
 from tkinter import *
 
-
+# Main window is created, name, icon and size are all specified.
 main_window = tkinter.Tk()
 main_window.wm_iconbitmap('images/icon.ico')
 main_window.title("PZ Server Mod Force Updater")
 main_window.geometry("380x140+10+20")
 
+# Variables to be used are declared.
 drive_var = tkinter.StringVar()
 drive_entry = tkinter.Entry(main_window, textvariable=drive_var)
 
 
+# This function is called after the Spiffo submit button is clicked, input is taken and used to call path utility
+# methods to create the two paths needed for the force update function to use to force update mods.
 def submit():
     output_utilities.please_wait()
 
@@ -28,13 +31,16 @@ def submit():
     output_utilities.alert(file_folder)
 
 
+# Main window widgets are declared and given the necessary info to function in the main window.
 main_lbl = Label(main_window, text=output_utilities.welcome_message(), font=("calibre", 10, "bold"))
-
 spiffo = PhotoImage(file="images/spiffo-button.png")
 submit_btn = tkinter.Button(main_window, command=submit, image=spiffo)
 
+# Main window widgets are placed on the window with .pack so that they stay in their assigned position interdependent
+# of window size.
 main_lbl.pack(side="top")
 drive_entry.pack(side="top")
 submit_btn.pack(side="bottom")
 
+# The main window is run.
 main_window.mainloop()
--- output_utilities.py
+++ output_utilities.py
@@ -1,15 +1,18 @@
 from tkinter import messagebox
 
 
+#  This function returns a welcome message to users.
 def welcome_message():
     return str("Type the name of your drive where your SteamLibrary is in." +
                "\n" +
                "Click on Spiffo to force update your Server mods!")
 
 
+#  This function returns an info alert with a string that is provided to the function.
 def alert(some_string):
     messagebox.showinfo("Hello", some_string)
 
 
+# This function displays a message box to let the user know the program is running.
 def please_wait():
     messagebox.showinfo("Running", "Please wait.")
