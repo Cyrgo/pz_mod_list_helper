fc9b057254cfac1d0818ec19cc441b54deec84ac
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-26 02:27:41 -0600

    Ver 0.2: Simplifies UI for the program to single button.

--- ui.py
+++ ui.py
@@ -7,7 +7,7 @@ import output_utilities
 main_window = tkinter.Tk()
 main_window.wm_iconbitmap('icon.ico')
 main_window.title("PZ Server Mod Force Updater")
-main_window.geometry("600x400+10+20")
+main_window.geometry("360x140+10+20")
 
 # Variables to hold string paths.
 client_workshop_file_path = tkinter.StringVar()
@@ -29,18 +29,11 @@ def submit():
     alert()
 
 
-cwfp_label = tkinter.Label(main_window, text="Client Workshop File Path:")
-cwfp_entry = tkinter.Entry(main_window, textvariable=client_workshop_file_path)
-swfp_label = tkinter.Label(main_window, text="Server Workshop Folder Path:")
-swfp_entry = tkinter.Entry(main_window, textvariable=server_workshop_folder_path)
-submit_btn = tkinter.Button(main_window, text="Submit", command=submit)
+spiffo = PhotoImage(file="spiffo-button.png")
+submit_btn = tkinter.Button(main_window, text="Submit", command=submit, image=spiffo)
 
 main_lbl = Label(main_window, text=output_utilities.welcome_message(), font=("calibre", 10, "bold"))
 main_lbl.pack(side="top")
-cwfp_label.pack(side="left")
-cwfp_entry.pack(side="left")
-swfp_label.pack(side="right")
-swfp_entry.pack(side="right")
 submit_btn.pack(side="bottom")
 
 main_window.mainloop()
