1daa39b8e4138c9f65dd0518f4790f6dafab3c8c
Cyrgo <76990907+Cyrgo@users.noreply.github.com>
2022-01-25 21:35:23 -0600

    Ver 0.2: Removes mod list helper functionality since the goal of this branch is to create a simple program with one button and all the work happens behind the scene.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import mod_list_helper
 import mod_update_fixer
-
-mod_list_helper.mod_list_diagnostic()
 
 mod_update_fixer.force_update()
--- path_utilities.py
+++ path_utilities.py
@@ -1,13 +1,3 @@
-# This function takes in string path of Project Zomboid Server Config Ini and returns it to be used.
-def ini_path_input():
-    print("Enter the path of your Project Zomboid Server Configuration ini file.")
-    print("Best to open the file from file explorer,")
-    print("copy paste the address, and add the Server Configuration ini file name.")
-    print("Example: C:\\Users\\cyrgo\\Zomboid\\Server\\31 Days Later-With Adi.ini")
-    ini_path = input()
-    return ini_path
-
-
 # This function takes in the string path of Project Zomboid Client Steam Workshop Content File and returns it to be
 # used.
 def client_workshop_path_input():
